ca8533999b0fb1e16bad985b4895da823872ef47
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-03 16:08:05 +0530

    Done

---
+++ Day5/main.py
@@ -0,0 +1,9 @@
+'''
+Dont skip this code if you think 
+its not neccesary then dont read it
+ok thank u
+'''
+print("hey i am a good boy and this\nviewer is also a good boy")
+
+print("Hello All", 6, 8, sep="~")
+print(27, 9, 2002, sep="-")
