4d90c9d74ff7983df847f3f4154cefbde5a9e253
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-03 20:57:55 +0530

    Done

---
+++ Day6/main.py
@@ -0,0 +1,27 @@
+# Data Types in Python
+a = 1.2
+b = "Akram"
+c = True
+d = None
+g = complex(8, 2)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(g))
+
+# List in Python (List  are mutable which can be changed)
+list1 = [8, 2.3, [-4, 5], ["Akram", "Hello"]]
+print(list1)
+
+
+# Tuples in Python (Tuples are immutable which can't be changed)
+tuple1 = [("Lion", "Tiger"), ("Ball", "Bat")]
+print(tuple1)
+
+# Dict (JavaScript ka jSON) data will be in key:value pairs
+dict1 = {"name": "ad", "age": 20, "canVote": True}
+print(dict1)
+
+
+# EVERYTHING IS OBJECT IN PYTHON
