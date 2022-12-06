d66c18c00cafc941d657bf1dce54d5a37a9cc5d9
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-06 07:03:06 +0530

    Done

---
+++ Day8/calculater.py
@@ -0,0 +1,6 @@
+a = 10
+b = 7
+print("addition of", a, "+", 7, "is : ", a+b)
+print("substraction of", a, "-", 7, "is : ", a-b)
+print("multiplication of", a, "*", 7, "is : ", a*b)
+print("devision of", a, "/", 7, "is : ", a/b)
