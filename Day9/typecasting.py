808267f07bb9c134281b0561dc2651213bcdb182
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-07 00:23:22 +0530

    Done

---
+++ Day9/typecasting.py
@@ -0,0 +1,15 @@
+# Conversion of one type of data into other type of data is known as Type-Casting
+
+a = "10"
+b = "2"
+
+print(int(a) - int(b))
+print(float(a) - float(b))
+# print(hex(a) - hex(b))
+# print(oct(a) - oct(b))
+
+# Implicit Type Casting
+
+c = 1.9
+d = 10
+print(c+d)
