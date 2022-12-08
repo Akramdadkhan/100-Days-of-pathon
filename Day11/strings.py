cb37c84b4a5303e1011d101e1c461fc6f32a1530
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-08 20:01:38 +0530

    Done

---
+++ Day11/strings.py
@@ -0,0 +1,18 @@
+# What are Strings
+
+name = "AD"
+surrName = "Khan"
+
+eat = '''Hii my name is A
+i am from there 
+and i know 
+how to make programs
+in python 
+'''
+print(eat)
+
+print(name[0])
+print(name[1])
+
+for charecter in eat:
+    print(charecter)
