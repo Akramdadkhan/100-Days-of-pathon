cd0d4345400f0804753d3b91a7a71f48a1c73fc6
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-08 19:44:55 +0530

    Done

---
+++ Day10/input.py
@@ -0,0 +1,16 @@
+# Taking User Input in Python
+
+a = input("Tell Us Your Name: ")
+if a == "Hello":
+    print("You are normal ")
+else:
+    print("Hii", a)
+
+
+x = input("Write First Number:")
+z = input("Write Second Number:")
+print(int(x) + int(z))
+print(int(x) - int(z))
+print(int(x) / int(z))
+print(int(x) * int(z))
+print(int(x) % int(z))
