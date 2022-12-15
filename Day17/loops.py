bfb06bcb66d2e530ba2a4c708d09cbd126fa66d4
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-15 08:33:01 +0530

    Done

---
+++ Day17/loops.py
@@ -0,0 +1,12 @@
+# Loops in Python
+name = "AD"
+for i in name:
+    print(i)
+
+for k in range(20):
+    print(k+1)
+
+
+# In this program it taking value og one first and till the range of 25-1=24 its adding 6 in every new output
+for kk in range(1, 25, 6):
+    print(kk)
