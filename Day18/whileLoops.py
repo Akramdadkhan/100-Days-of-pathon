93064d34222728850208f4f7729b480b8dbc405f
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-15 18:56:12 +0530

    Done

---
+++ Day18/whileLoops.py
@@ -0,0 +1,24 @@
+# for i in range(5):
+#     print(i)
+
+# i = 0
+# while (i < 9):
+#     print(i)
+#     i = i+1
+
+# Condition base while loop
+
+# i = int(input("enter the number: "))
+# while (i <= 9):
+#     i = int(input("enter the number: "))
+#     print(i)
+
+# print("Loop completed")
+
+# Decremental while loop
+i = 10
+while (i > -1):
+    print(i)
+    i = i-1
+else:
+    print("i am in else")
