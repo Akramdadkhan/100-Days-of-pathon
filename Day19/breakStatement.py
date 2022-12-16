1cb4dee0a3e1e052583769bb3d76144b90cfade3
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-16 18:59:54 +0530

    Done

---
+++ Day19/breakStatement.py
@@ -0,0 +1,21 @@
+# Break Statement
+# for i in range(20):
+#     if (i == 10):
+#         break
+#     print("5 x", i+1, "=", 5*(i+1))
+# print("Loop End")
+
+# for i in range(20):
+#     if (i == 10 or i == 11 or i == 12):
+#         print("Skip the iteration")
+#         continue
+#     print("5 x", i+1, "=", 5*(i+1))
+
+# Do While in Python
+i = 0
+while True:
+
+    print(i)
+    i = i+1
+    if (i % 100 == 0):
+        break
