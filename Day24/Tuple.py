8dd859acaea91d4929bf0b3ef8d664a133f10a9f
AkramDadKhan1 <akram.khan@fyntune.com>
2022-12-22 01:02:28 +0530

    Done

---
+++ Day24/Tuple.py
@@ -0,0 +1,10 @@
+#Tuple in Python
+# It is same as list but it cant be changed and denoted by ()
+tup = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(type(tup), tup)
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[3])
+print(tup[4])
+print(tup[5])
